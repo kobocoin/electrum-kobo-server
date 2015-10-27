0408c7390bd5e484ddbc6e5e09d2de791af1313a
OKtoshi <devteam@okcash.co>
2015-10-27 06:06:19 -0500

    Update scripts

--- setup.py
+++ setup.py
@@ -3,30 +3,28 @@ from setuptools import setup
 setup(
     name="electrum-server",
     version="0.9",
-    scripts=['run_electrum_server','electrum-server'],
+    scripts=['run_electrum_server','electrum-server','configure','electrum.conf.sample'],
     install_requires=['plyvel','jsonrpclib', 'irc'],
-    package_dir={
-        'electrumserver':'src'
-        },
+    package_dir={'electrum_server':'src'},
     py_modules=[
-        'electrumserver.__init__',
-        'electrumserver.utils',
-        'electrumserver.storage',
-        'electrumserver.deserialize',
-        'electrumserver.networks',
-        'electrumserver.blockchain_processor',
-        'electrumserver.server_processor',
-        'electrumserver.processor',
-        'electrumserver.version',
-        'electrumserver.ircthread',
-        'electrumserver.stratum_tcp',
-        'electrumserver.stratum_http'
+        'electrum_server.__init__',
+        'electrum_server.utils',
+        'electrum_server.storage',
+        'electrum_server.deserialize',
+        'electrum_server.networks',
+        'electrum_server.blockchain_processor',
+        'electrum_server.server_processor',
+        'electrum_server.processor',
+        'electrum_server.version',
+        'electrum_server.ircthread',
+        'electrum_server.stratum_tcp',
+        'electrum_server.stratum_http'
     ],
     description="OKCash Electrum Server",
-    author="Thomas Voegtlin",
+    author="Thomas Voegtlin, OKtoshi",
     author_email="thomasv1@gmx.de",
     license="GNU Affero GPLv3",
-    url="https://github.com/spesmilo/electrum-server/",
+    url="https://github.com/oktoshi/electrum-server/",
     long_description="""Server for the Electrum Lightweight OKCash Wallet"""
 )
 
