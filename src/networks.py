23f816db42ffd2e016255ab898cf2abc4daa76a4
TheTribesman <kobocoin@users.noreply.github.com>
2016-04-02 02:45:02 +0100

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -1,14 +1,14 @@
 # Main network and testnet3 definitions
 
 params = {
-    'okcash_main': {
-        'pubkey_address': 55,
+    'Kobocoin_main': {
+        'pubkey_address': 35,
         'script_address': 28,
-        'genesis_hash': '0000046309984501e5e724498cddb4aff41a126927355f64b44f1b8bba4f447e'
+        'genesis_hash': '00000c98be708f9ff86319738d4ce8778f1db7ce140d2d665cca9281eef433bd'
     },
-    'okcash_test': {
-        'pubkey_address': 68,
-        'script_address': 73,
+    'Kobocoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
         'genesis_hash': '0000e3283629707a14a6c5f3297995095ac0e337b2af9bca1358d4788ed86169'
     }
 }
